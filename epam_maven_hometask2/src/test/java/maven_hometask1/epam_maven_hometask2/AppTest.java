f3c863be2a2d90bf9ca6fd53725b2936cab9485b
praveen-coder <ellkollapraveengoud1998@gmail.com>
2020-02-07 19:24:00 +0530

    first commit

---
+++ epam_maven_hometask2/src/test/java/maven_hometask1/epam_maven_hometask2/AppTest.java
@@ -0,0 +1,20 @@
+package maven_hometask1.epam_maven_hometask2;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
